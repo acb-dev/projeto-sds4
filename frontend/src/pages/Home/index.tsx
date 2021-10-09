d6d70363335dd39aa876afb4147dc2d4f9948525
acb-dev <acbdev@hotmail.com>
2021-10-09 15:48:39 -0300

    Home, Navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,30 @@
+import Footer from "assets/components/Footer";
+import NavBar from "assets/components/NavBar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
-return (
-<div>
-    <h1>Página Home</h1>
-</div>
+    return (
+        <>
+        <NavBar />
+        <div className="container">
+
+            <div className="jumbotron">
+
+                <h1 className="display-4">DSVendas</h1>
+
+                <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+
+                <hr />
+
+                <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                <Link className="btn-primary btn-lg" to="dashboard">
+                        Acessar o dashboard
+                </Link>
+            </div>
+
+        </div>
+        <Footer />
+        </>
 );
 }
 
